1bc32e767d91b25c12a1ccbd63c6211fb79aff10
Roshni Kumari <roshni.kumari@myntra.com>
2020-01-12 16:33:41 +0530

    Levelwidth

--- tree.js
+++ tree.js
@@ -33,6 +33,7 @@ class Tree{
         }
     }
 }
+
 function treeLevelWidth(rootnode){
 let counterArry  = [0];
 let outArry = [rootnode, 'done'];
@@ -50,6 +51,28 @@ while(outArry.length){
 }
 return counterArry;
 }
+
+
+//solution 2
+function levelWidth(root) {
+    const arr = [root, 's'];
+    const counters = [0];
+  
+    while (arr.length > 1) {
+      const node = arr.shift();
+  
+      if (node === 's') {
+        counters.push(0);
+        arr.push('s');
+      } else {
+        arr.push(...node.children);
+        counters[counters.length - 1]++;
+      }
+    }
+  
+    return counters;
+  }
+  
 
 const letters = [];
 const t = new Tree();
